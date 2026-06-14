460da281ccd7b9bbec738556bd45e9935a292885
Rafael Emerick <rafael.rezo@gmail.com>
2026-06-14 10:43:01 -0300

    Reseta tema ReadTheDocs para padrão

--- docs/javascripts/mermaid-init.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  if (window.mermaid) {
-    window.mermaid.initialize({
-      startOnLoad: true,
-      theme: "default",
-      securityLevel: "loose"
-    });
-  }
-});
